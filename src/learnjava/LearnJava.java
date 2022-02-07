f0f78e02e0df5cbf6395a7b5af62f153ab6e3729
Rishabh Agrawal <83543522+Rishabhco@users.noreply.github.com>
2022-02-07 15:16:56 +0530

    learnt take user input and one dimension array

--- src/learnjava/LearnJava.java
+++ src/learnjava/LearnJava.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package learnjava;
+import java.util.*;
 /**
  *
  * @author risha
@@ -20,5 +21,21 @@ public class LearnJava {
         System.out.println(a);
         int c=il.intValue();
         System.out.println(c);
+
+        //Take User Input
+        Scanner sc=new Scanner(System.in);
+        int age=sc.nextInt();
+        System.out.println(age);
+
+        //Make One dimension array
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+        
     }
 }
